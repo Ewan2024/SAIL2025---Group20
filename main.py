a435aa1afbefbcea862033464b6a9cd4512fa8ec
Ewan2024 <ewanbrett2002@gmail.com>
2025-10-06 11:24:22 +0200

    added map to main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from streamlit_folium import st_folium
+import folium
 
 st.set_page_config(
     page_title="Crowd Management Dashboard",
@@ -9,7 +11,15 @@ st.set_page_config(
 def main():
     st.title("SAIL Amsterdam Crowd Management Dashboard")
 
+    st.sidebar.header("Filters")
+
+    map_style = st.sidebar.selectbox(
+        "Map Style",
+        ["OpenStreetMap", "Stamen Terrain", "CartoDB positron"]
+    )
+
+    m = folium.Map(location=[52.3791, 4.9003], zoom_start=11, tiles=map_style)
+    st_data = st_folium(m, width=700, height=500)
 
 if __name__ == "__main__":
     main()
-
